a3773521e69c3c67782c95656cf22b3b2c7f23b4
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-28 11:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function showQuestion(){
 }
 
 function showResult(){
-	questionElement.textContent = `\n You answered ${score}/4 questions correctly\n`;
+	questionElement.textContent = `\n You answered ${score}/${questions.length} questions correctly\n`;
 	aTextElement.style.display = "none";
 	bTextElement.style.display = "none";
 	cTextElement.style.display = "none";
